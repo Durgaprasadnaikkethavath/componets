038b7e8c8e76a8d1755db9d7d2ff257de610d3c4
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-12-23 22:36:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>components</h1>
+      <h1>function components and class components</h1>
+    </>
+  );
+};
+
+export default App;
